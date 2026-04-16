8020a48670ced81b60a8d111192a38a94b784e30
Hemant Kumar <146334253+Hemant-kalia@users.noreply.github.com>
2026-04-16 07:30:00 +0000

    removed duplicate lines from index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Header from './MyComponents/Header';
-import Footer from './MyComponents/Footer';
-import Home from './MyComponents/Home';
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
