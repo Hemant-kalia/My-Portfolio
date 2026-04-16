b7c2d1bd7f3d731b690efdafe4e3bca9c3eb853a
Hemant Kumar <146334253+Hemant-kalia@users.noreply.github.com>
2026-04-16 07:27:04 +0000

    Fixed App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import Header from './MyComponents/Header';
 import Footer from './MyComponents/Footer';
+import Home from './MyComponents/Home';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* your other routes */}
+      </Routes>
       <Footer />
-    </>
+    </HashRouter>
 
   );
 }
 
 export default App;
+;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Header from './MyComponents/Header';
+import Footer from './MyComponents/Footer';
+import Home from './MyComponents/Home';
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/My-Portfolio">
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
