50833025c5dbddec57dd3fb9223a79bd3a26190b
Hemant Kumar <146334253+Hemant-kalia@users.noreply.github.com>
2026-04-16 06:56:09 +0000

    Fixed homepage loading

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <div className="content">
-      <App />
-    </div>
-    
-  </React.StrictMode>
+  <BrowserRouter basename="/My-Portfolio">
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
